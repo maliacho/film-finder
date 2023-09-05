7306e79b9550caf6e61d256c6a188da089e52780
maliacho <maliacho21@gmail.com>
2023-09-05 14:06:32 -0700

    created empty function redirectUser that aims to redirect the user to a new page after searching for a movie. created an event listener for when the user clicks on the search button.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function movieInfo(search){
+let searchButton = document.querySelector('search-results');
+
+function movieInfo(search) {
     // creates a div in HTML to display movie information
     let resultInfo = document.createElement('div');
     resultInfo.classList.add(); // @@TODO CSS parameters needed for styling
@@ -17,11 +19,19 @@ function movieInfo(search){
 
     // creates a p tag for the Rotten Tomatoes Score
     let tomatoScore = document.createElement('p');
-    tomatoScore.textContent = search.rotten-tomatoes
+    tomatoScore.textContent = search.rotten - tomatoes;
 
+};
+
+// function playTrailer() {
+//      // creates a link to the movie trailer
+//  let trailerEl = document.createElement('href')
+
+// }
+
+function redirectUser() {
     
-
 }
 
- // creates a link to the movie trailer
- let trailerEl = document.createElement('href')
+searchButton.addEventListener('click', redirectUser)
+
